e93fb8d597df8cd37f7e889ad74799323869af15
Mathou54 <matt54425+github.com@gmail.com>
2018-01-10 12:48:13 +0100

    Add foxy_lazer animation.

--- fl-ui/src/app/home/home.component.ts
+++ fl-ui/src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public isInPicture: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
