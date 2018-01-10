a15f3897dd36c4cc738ae5285aad071f6a632c5b
Bogdan Mata <bogdanmata@gmail.com>
2018-01-10 14:56:49 +0100

    The form + buttons

--- fl-ui/src/app/app.module.ts
+++ fl-ui/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatDatepickerModule,  } from '@angular/material/datepicker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSelectModule } from '@angular/material/select';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { SectionRbcComponent } from './section-rbc/section-rbc.component';
@@ -36,7 +37,7 @@ const appRoutes: Routes = [
       { enableTracing: false } // <-- debugging purposes only
     ),
     MatTableModule, MatRadioModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatInputModule,
-    BrowserAnimationsModule, MatSelectModule, MatAutocompleteModule, FormsModule, ReactiveFormsModule
+    BrowserAnimationsModule, MatSelectModule, MatAutocompleteModule, MatButtonModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
