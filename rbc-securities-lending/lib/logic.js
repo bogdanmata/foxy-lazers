2e1f7ead4facf0efd4376e0130930af067aa66c6
jscaze <js.adresse.pro@gmail.com>
2018-01-10 13:55:06 +0100

    init setup

--- rbc-securities-lending/lib/logic.js
+++ rbc-securities-lending/lib/logic.js
@@ -4,11 +4,24 @@
  */
 
 /**
- * Sample transaction
- * @param {com.rbc.hackathon.ChangeAssetValue} changeAssetValue
+ * A request for Sec lending has been received
+ * @param {com.rbc.hackathon.LendingRequest} lendingRequest - the LendingRequest transaction
  * @transaction
  */
-function onChangeAssetValue(changeAssetValue) {
+async function onSecurityLendingReception(lendingRequest) {  // eslint-disable-line no-unused-vars
+
+    const instrument = lendingRequest.instrument;
+    const borrower   = lendingRequest.borrower;
+
+    console.log('Lending request received on instrument ' + instrument.isin + ' done by ' + borrower.name);
+}
+
+/**
+ * A offer for Sec lending has been sent
+ * @param {com.rbc.hackathon.LendingOffer} lendingOffer
+ * @transaction
+ */
+function onChangeAssetValue(lendingOffer) {
     var assetRegistry;
     var id = changeAssetValue.relatedAsset.assetId;
     return getAssetRegistry('com.rbc.hackathon.SampleAsset')
@@ -20,4 +33,34 @@ function onChangeAssetValue(changeAssetValue) {
             asset.value = changeAssetValue.newValue;
             return assetRegistry.update(asset);
         });
+}
+
+/**
+ * Initialize some test assets and participants useful for running a demo.
+ * @param {com.rbc.hackathon.SetupDemo} setupDemo - the SetupDemo transaction
+ * @transaction
+ */
+async function setupDemo(setupDemo) {  // eslint-disable-line no-unused-vars
+
+    const factory = getFactory();
+    const NS = 'com.rbc.hackathon';
+
+    // create the borrowers
+    const borrower1 = factory.newConcept(NS, 'Borrower1');
+    borrower1.accountBalance = 2000;
+
+    const borrower = factory.newConcept(NS, 'Borrower2');
+    borrower2.accountBalance = 2000;
+
+    const Bond = factory.newConcept(NS, 'GTDEM2Y:GOV', 'Germany Bund 2 Year Yield');
+    const Bond = factory.newConcept(NS, 'GTDEM5Y:GOV', 'Germany Bund 5 Year Yield');
+    const Bond = factory.newConcept(NS, 'GTDEM10Y:GOV', 'Germany Bund 10 Year Yield');
+
+    // create the bank
+    const bank = factory.newResource(NS, 'RBC Investor and Treasuary Services');
+ 
+    
+    growerAddress.country = 'USA';
+    grower.address = growerAddress; 
+    grower.accountBalance = 0;
 }
