0e7ceb13ccc405d14f11bbd99c555d14e864066e
timzok <kader.timzorane@gmail.com>
2018-01-10 14:59:56 +0100

    fix js issue

--- rbc-securities-lending/lib/logic.js
+++ rbc-securities-lending/lib/logic.js
@@ -8,10 +8,10 @@
  * @param {com.rbc.hackathon.LendingRequest} lendingRequest - the LendingRequest transaction
  * @transaction
  */
-async function onSecurityLendingReception(lendingRequest) {  // eslint-disable-line no-unused-vars
+function onSecurityLendingReception(lendingRequest) {  // eslint-disable-line no-unused-vars
 
-    const instrument = lendingRequest.instrument;
-    const borrower   = lendingRequest.borrower;
+    var instrument = lendingRequest.instrument;
+    var borrower   = lendingRequest.borrower;
 
     console.log('Lending request received on instrument ' + instrument.isin + ' done by ' + borrower.name);
 }
@@ -21,9 +21,9 @@ async function onSecurityLendingReception(lendingRequest) {  // eslint-disable-l
  * @param {com.rbc.hackathon.LendingOffer} lendingOffer - the lendingOffer transaction
  * @transaction
  */
-async function onLendingOfferReception(lendingOffer) {  // eslint-disable-line no-unused-vars
+function onLendingOfferReception(lendingOffer) {  // eslint-disable-line no-unused-vars
 
-    const bank   = lendingRequest.bank;
+    var bank   = lendingRequest.bank;
 
     console.log('Lending offer received from '  + bank.name);
 }
@@ -34,43 +34,43 @@ async function onLendingOfferReception(lendingOffer) {  // eslint-disable-line n
  * @param {com.rbc.hackathon.SetupDemo} setupDemo - the SetupDemo transaction
  * @transaction
  */
-async function setupDemo(setupDemo) {  // eslint-disable-line no-unused-vars
+function setupDemo(setupDemo) {  // eslint-disable-line no-unused-vars
 
-    const factory = getFactory();
-    const NS = 'com.rbc.hackathon';
+    var factory = getFactory();
+    var NS = 'com.rbc.hackathon';
 
     console.log('Creating Borrowers');
 
     // create the borrowers
-    const borrower1 = factory.newResource(NS, 'Borrower', 'borrower1');
+    var borrower1 = factory.newResource(NS, 'Borrower', 'borrower1');
     borrower1.accountBalance = 2000;
 
-    const borrower2 = factory.newResource(NS, 'Borrower', 'borrower2');
+    var borrower2 = factory.newResource(NS, 'Borrower', 'borrower2');
     borrower2.accountBalance = 2000;
 
     console.log('Creating Bonds');
 
-    const bond2  = factory.newResource(NS, 'Bond','GTDEM2Y:GOV');
+    var bond2  = factory.newResource(NS, 'Bond','GTDEM2Y:GOV');
     bond2.description='Germany Bund 2 Year Yield';
-    const bond5  = factory.newResource(NS, 'Bond', 'GTDEM5Y:GOV');
+    var bond5  = factory.newResource(NS, 'Bond', 'GTDEM5Y:GOV');
     bond5.description = 'Germany Bund 5 Year Yield';
-    const bond10 = factory.newResource(NS, 'GTDEM10Y:GOV');
+    var bond10 = factory.newResource(NS, 'GTDEM10Y:GOV');
     bond10.description = 'Germany Bund 10 Year Yield';
 
 
     console.log('Creating Banks');
     // create the banks
-    const bank1 = factory.newResource(NS, 'bank' ,'RBC Investor and Treasuary Services');
+    var bank1 = factory.newResource(NS, 'bank' ,'RBC Investor and Treasuary Services');
     bank1.accountBalance = 2000;
     
 
     console.log('___Creating Porfolio bank1');
     // portfolio creation
-    const portfolioItem_RBC1 = factory.newConcept(NS, 'PortfolioItem');
+    var portfolioItem_RBC1 = factory.newConcept(NS, 'PortfolioItem');
     portfolioItem_RBC1.instrument = factory.newRelationship(NS, 'Bond', 'GTDEM2Y:GOV');
     portfolioItem_RBC1.quantity = 200 ;
 
-    const portfolioItem_RBC2 = factory.newConcept(NS, 'PortfolioItem');
+    var portfolioItem_RBC2 = factory.newConcept(NS, 'PortfolioItem');
     portfolioItem_RBC2.instrument = factory.newRelationship(NS, 'Bond', 'GTDEM5Y:GOV');
     portfolioItem_RBC2.quantity = 1000 ;
 
@@ -80,9 +80,9 @@ async function setupDemo(setupDemo) {  // eslint-disable-line no-unused-vars
     bank1.portfolio.push(portfolioItem_RBC2);
 
 
-    const bank2 = factory.newResource(NS, 'bank', 'bank2');
+    var bank2 = factory.newResource(NS, 'bank', 'bank2');
     bank2.accountBalance = 2000;
-    const portfolioItem1 = factory.newConcept(NS, 'PortfolioItem');
+    var portfolioItem1 = factory.newConcept(NS, 'PortfolioItem');
     portfolioItem1.instrument = factory.newRelationship(NS, 'Bond', 'GTDEM2Y:GOV');
     portfolioItem1.quantity = 10 ;
 
