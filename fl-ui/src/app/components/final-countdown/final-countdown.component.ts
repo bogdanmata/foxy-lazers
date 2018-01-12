16cee61e7cf31f3e7c4e8f77b4ad3c97ac229840
Mathou54 <matt54425+github.com@gmail.com>
2018-01-12 08:40:05 +0100

    Remove 0 left in countdown.

--- fl-ui/src/app/components/final-countdown/final-countdown.component.ts
+++ fl-ui/src/app/components/final-countdown/final-countdown.component.ts
@@ -42,7 +42,15 @@ export class FinalCountdownComponent implements OnInit {
     let minutes = Math.floor((secondsToGo % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((secondsToGo % (1000 * 60)) / 1000);
 
-    return `${days}days ${hours}h ${minutes}m ${seconds}s left.`;
+    if (days > 0) {
+      return `${days}days ${hours}h ${minutes}m ${seconds}s left.`;
+    } else if (hours > 0) {
+      return `${hours}h ${minutes}m ${seconds}s left.`;
+    } else if (minutes > 0) {
+      return `${minutes}m ${seconds}s left.`;
+    } else {
+      return `${seconds}s left.`;
+    }
   }
 
 }
